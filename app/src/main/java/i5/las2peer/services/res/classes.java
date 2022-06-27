11a33a17ebeae62b3d8822d49edf96e8f5e94a78
PhilCAEUser <mail@rwth>
2022-06-27 17:39:15 +0000

    test schema

--- app/src/main/java/i5/las2peer/services/res/classes.java
+++ app/src/main/java/i5/las2peer/services/res/classes.java
@@ -6,6 +6,35 @@ import org.json.simple.parser.ParseException;
 
 public class classes {
 
+    class User {
+
+    public User() {}
+    
+    private String username;
+
+    public void setusername(String setValue) {
+        this.username = setValue;
+    }
+
+    public String getusername() {
+        return this.username;
+    }
+
+    public JSONObject toJSON() {
+
+        JSONObject jo = new JSONObject();
+        jo.put("username", this.username); 
+
+        return jo;
+    }
+
+    public void fromJSON(String jsonString) throws ParseException {
+        JSONObject jsonObject = (JSONObject) JSONValue.parseWithException(jsonString);
+        this.username = (String) jsonObject.get("username"); 
+
+    }
+
+}
 
     
 }
